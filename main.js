8d6d644d7486aed34f720fefadb31917c39fa8a8
KeisGSmit <kgsmit98@gmail.com>
2020-07-26 12:45:54 +0000

    created a new function to move the data variable

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ var data;
 xhr.open('GET', "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
 
+function setData(jsonData){
+    data = jsonData
+    console.log(data)
+}
 xhr.onreadystatechange = function (){
     if (this.readyState == 4 && this.status == 200){
-        data = this.responseText;
+        setData(JSON.parse(this.responseText));
     }
-}
-
-console.log(data);
+};
